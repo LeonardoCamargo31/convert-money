e88805f24dd846fe82482b3e91fc1253206bee55
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2019-04-20 14:14:42 -0300

    Adicionado arquivo now.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000
 
 const { convert, toMoney } = require('./lib/convert')
 
